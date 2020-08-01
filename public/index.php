57f7b3c63d7207f72e51836f799f9d5d7a86396c
Cxb <851145971@qq.com>
2020-08-01 13:12:01 +0800

    添加自动注册

--- public/index.php
+++ public/index.php
@@ -9,7 +9,9 @@
 
 // 定义常量
 define('FRAME_BASE_PATH', __DIR__ . '/../');
+// 环境常量
 define('IS_CLI', PHP_SAPI == 'cli' ? true : false);
+define('IS_WIN', strpos(PHP_OS, 'WIN') !== false);
 
 //加载函数库
 $file = FRAME_BASE_PATH . 'vendor/autoload.php';
@@ -18,9 +20,24 @@ if (file_exists($file)) {
 } else {
     die("include composer autoload.php fail");
 }
-if (file_exists(FRAME_BASE_PATH.'app.php')) {
-    $app = require FRAME_BASE_PATH.'app.php';
-}else{
+
+spl_autoload_register(function ($class) {
+    if (strpos($class, 'core\\') === 0) {
+        $name = substr($class, strlen('core'));
+        $classPath = FRAME_BASE_PATH . "core" . strtr($name, '\\', DIRECTORY_SEPARATOR) . '.php';
+        if (file_exists($classPath)) {
+            include $classPath;
+            return true;
+        } else {
+            return false;
+        }
+    }
+});
+
+
+if (file_exists(FRAME_BASE_PATH . 'app.php')) {
+    $app = require FRAME_BASE_PATH . 'app.php';
+} else {
     die("include frame app.php fail");
 }
 
