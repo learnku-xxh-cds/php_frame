4ed9ab7e3ec8c0391d8308489f549470d828c8a6
xxh <449399575@qq.com>
2020-08-08 15:46:10 +0800

    删除缓存文件

--- views/cache/30bc39a920556afe1cdc9911a5c6c7d8548738ed.php
+++
@@ -1,2 +0,0 @@
-
-<h1> FOOTER </h1><?php /**PATH E:\code\php_frame\views/blade/footer.blade.php ENDPATH**/ ?>
--- views/cache/4e4803d3580b76e18c428d9d93d03d80e2c44d4d.php
+++
@@ -1,3 +0,0 @@
-
-
-这是blade模板<?php /**PATH E:\code\php_frame\views/index.blade.php ENDPATH**/ ?>
